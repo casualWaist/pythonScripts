ffb008dd9a28e6c8d4cb135746b99e46dea3f352
casualWaist <119624750+casualWaist@users.noreply.github.com>
2023-01-07 12:01:00 -0500

    dumb but absolutely the best thing I ever did
    All the script does is open a window that lets a user click on a job file and have the file path from a certain directory copied to the clipboard.
    This is absolutely a hack, but we used the file path as the job title stored in our job databases. This script eliminated the chance that the path would be entered incorrectly and was the foundation of every automation we enacted after that.

--- PathLoader.py
+++ PathLoader.py
@@ -1,3 +1,7 @@
+# All the script does is open a window that lets a user click on a job file and have the file path from a certain directory copied to the clipboard.
+# This is absolutely a hack, but we used the file path as the job title stored in our job databases.
+# This script eliminated the chance that the path would be entered incorrectly and was the foundation of every automation we enacted after that. 
+
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import os
